a47770d0f44d7a1a503e22e610d3f1c5ed924fc9
Bow-Pixels <bowpixels@gmail.com>
2025-03-07 13:04:36 -0500

    Entity and Service Class Finished

--- Basketball.java
+++ Basketball.java
@@ -3,11 +3,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 public class Basketball{
-    public ArrayList<Team> teams = new ArrayList<Team>();
+    private ArrayList<Team> teams = new ArrayList<Team>();
 
-
-    public static void main(String[] args){
-        Basketball org = new Basketball();
+    public Basketball(){
         FileOperator file = new FileOperator("data/arenas.txt");
         String[] arena_names = file.toStringArray("data/arenas.txt", 30);
         String[] locations = file.toStringArray("data/locations.txt", 30);
@@ -15,145 +13,43 @@ public class Basketball{
         int[] capacities = file.toIntArray("data/capacities.txt", 30);
         int[] championships = file.toIntArray("data/championships.txt", 30);
         for(int i = 0; i < arena_names.length; i++){
-            org.teams.add(new Team(team_names[i], locations[i], arena_names[i], championships[i], capacities[i]));
+            teams.add(new Team(team_names[i], locations[i], arena_names[i], championships[i], capacities[i]));
         }
-        System.out.println(org.teams.get(0));
+    }
 
 
-        
-
-        
+    public static void main(String[] args){
        
+    }
+    
+    public ArrayList<Team> getTeams(){
+        return this.teams;
+    }
 
-    }
-    public static void printArray(int[] nums){
-        System.out.print("[");
-        for(int i = 0; i < nums.length-1; i++){
-            System.out.print(nums[i] + ", ");
-        }
-        System.out.print("]");
-    }
-     // Linear Search  
-    public static int searchList(int[] numbers, int target){
-        int index = 0;
-        while(index <= numbers.length - 1){
-            if(numbers[index] == target){
-                return index;
-            }
-            index++;
-        }
-
-        return -1;
-    }
-    // Binary Search - List must be sorted first
-    public static int searchList(int target, int[] numbers){
-        int minIndex = 0;
-        int maxIndex = numbers.length -1;
-        while (minIndex <= maxIndex){
-            int middleIndex = (maxIndex+minIndex)/2;
-            if(numbers[middleIndex] == target){
-                return middleIndex;
-            }
-            else{
-                if(target > numbers[middleIndex]){
-                    minIndex = middleIndex + 1;
-                }else{
-                    maxIndex = middleIndex - 1;
-                }
+    public static int arenasbyTeam(String target){
+        for(Team team : teams){
+            if(team.getName().equals(target)){
+            return team.getID();
             }
         }
         return -1;
     }
-    public static int[] reverseList(int[] array){
-        int[] newArray = new int[array.length];
-
-        for(int i = 0; i < array.length-1; i++){
-            newArray[i] = array[array.length-1-i];
-        }
-        return newArray;
-    }
-
-    public static int findMax(int[] values){
-        int max = values[0];
-        for(int i = 0; i < values.length; i++){
-            if(values[i] > max){
-                max = values[i];
-            }
-        }
-        return max;
-    }
-
-    public static int[] ascendOrDescend(int[] arr, int flip){
-        // if flip = 1, sorts in ascending order
-        // if flip = 0, sorts in descending order
-        int temp;
-        for(int i = 0; i < arr.length-1; i++){
-            for(int j = i+1; j< arr.length; j++){
-                if (arr[j] > arr[i]){
-                    temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                }
-            }
-        }
-        if(flip == 1){
-            return reverseList(arr);
-        }
-        else{
-            return arr;
-        }
-    }
-   
-    public static int[] findString(String[] list, String target){
-        ArrayList<Integer> indices = new ArrayList<>();
-        for(int i = 0; i < list.length; i++){
-            if(list[i].equals(target)){
-                indices.add(i);
-            }
-            
-        }
-        int[] result = new int[indices.size()];
-        for(int i = 0; i < indices.size(); i++){
-            result[i] = indices.get(i);
-        }
-        return result;
-    }
-
-    public static String[] arenasWith(String[] list, String target, FileOperator file){
-        int[] indices = findString(list ,target);
-        String[] arenaNames = file.toStringArray("data/arenas.txt", 30);
-        String[] arenas = new String[indices.length];
-        for(int i = 0; i< indices.length; i++){
-            arenas[i] = arenaNames[indices[i]];
-        }
-        return arenas;
-    }
-
-    public static String[] arenasByTeam(String target, FileOperator file){
-        String filepath = "data/teams.txt";
-        int arr_size = 30;
-
-        String[] foundList = file.toStringArray(filepath,arr_size);
-        String[] arenas = arenasWith(foundList, target, file);
-        return arenas;
-
-    }
 
 
-    public static String findMostCommonArena(FileOperator file) {
-       
-        ArrayList<String> locations = file.toStringArray("data/locations.txt");
+    public static int findMostCommonArena(FileOperator file) {
 
         List<String> locationList = new ArrayList<>();
         List<Integer> countList = new ArrayList<>();
         
-        for (String location : locations) {
-            if (locationList.contains(location)) {
-                int index = locationList.indexOf(location);
-                countList.set(index, countList.get(index) + 1);
-            } else {
-                locationList.add(location);
+        for (Team team: teams){
+            if(locationList.contains(team.getLocation())){
+                int index = locationList.indexOf(team.getLocation());
+                countList.set(index, countList.get(index) +1);
+            }
+            else{
+                locationList.add(team.getLocation());
                 countList.add(1);
+                
             }
         }
         
@@ -164,7 +60,8 @@ public class Basketball{
             }
         }
         
-        return locationList.get(maxIndex);
+        // return locationList.get(maxIndex);
+        return maxIndex;
     }
 
     public static String largestArena(FileOperator file){
@@ -183,51 +80,8 @@ public class Basketball{
 
     }
 
-    public static String[] minChampionships(FileOperator file){
-        ArrayList<String> min_teams = new ArrayList<String>();
-        ArrayList<Integer> min_championships = new ArrayList<Integer>();
-        String[] arenas = file.toStringArray("data/teams.txt", 30);
-        int[] championships = file.toIntArray("data/championships.txt", 30);
-
-        min_teams.add(arenas[0]);
-        min_championships.add(championships[0]);
-
-        for(int i = 0; i < arenas.length; i++){
-            if(championships[i] < min_championships.get(0)){
-                min_teams.clear();
-                min_championships.clear();
-                min_teams.add(arenas[i]);
-                min_championships.add(championships[i]);
-            }
-            else if(championships[i] == min_championships.get(0)){
-                min_teams.add(arenas[i]);
-                min_championships.add(championships[i]);
-            }
-            
-        }
-        return min_teams.toArray(new String[0]);
-    }
-
-    public static int[] capacityByCity(String target, FileOperator file){
-        String[] arenas = file.toStringArray("data/arenas.txt", 30);
-        ArrayList<Integer> final_capacities = new ArrayList<>();
-        int[] capacities = file.toIntArray("data/capacities.txt", 30);
-
-        for (int i = 0; i < arenas.length; i++){
-            if(arenas[i].equals(target)){
-                final_capacities.add(capacities[i]);
-            }
-        }
-        int[] new_capacities = new int[final_capacities.size()];
-        
-        for(int i = 0; i < new_capacities.length; i++){
-            new_capacities[i] = final_capacities.get(i);
-        }
-        return new_capacities;
-
-    }
-
-
+    
+     
 
 
 
--- DataGUI.java
+++ DataGUI.java
@@ -1,11 +1,16 @@
-import javax.swing.*;
 import java.awt.*;
-
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class DataGUI extends JFrame {
     private JTextField inputField;
     private JTextArea resultsArea;
     private FileOperator file = new FileOperator("data/arenas.txt");
+    private static Basketball ball = new Basketball();
+    public static int counter = 0;
     
     public DataGUI() {
         setTitle("Arena Data Analyzer");
@@ -16,6 +21,7 @@ public class DataGUI extends JFrame {
         inputField = new JTextField(20);
         JButton analyzeTeamButton = new JButton("Analyze by Team");
         JButton commonLocationButton = new JButton("Get Most Common Arena Location");
+        JButton saveScreenButton = new JButton("Save Screen");
         resultsArea = new JTextArea(10, 40);
         resultsArea.setEditable(false);
 
@@ -23,9 +29,11 @@ public class DataGUI extends JFrame {
         add(analyzeTeamButton);
         add(commonLocationButton);
         add(new JScrollPane(resultsArea));
+        add(saveScreenButton);
 
         analyzeTeamButton.addActionListener(e -> analyzeByTeam());
         commonLocationButton.addActionListener(e -> getMostCommonArena());
+        saveScreenButton.addActionListener(e -> saveScreen());
     }
 
     private static String stringify(String[] arr) {
@@ -34,6 +42,14 @@ public class DataGUI extends JFrame {
             result += s + "\n";
         }
         return result;
+    }
+    public static Team getTeam(int index){
+        for(Team team: ball.getTeams()){
+            if(index == team.getID()){
+                return team;
+            }
+        }
+        return ball.getTeams().get(0);
     }
     private static String stringify_int(int[] arr) {
         String result = "";
@@ -44,24 +60,36 @@ public class DataGUI extends JFrame {
     }
 
     private void analyzeByTeam(){
-        String[] arenas = DataAnalyzer.arenasByTeam((inputField.getText()),file);
+        int index = Basketball.arenasbyTeam(inputField.getText());
 
-        resultsArea.setText("Arenas by Team " + inputField.getText() + ": \n" + stringify(arenas) + "\n");
+        resultsArea.setText("Arenas by Team " + inputField.getText() + ": \n" + getTeam(index) + "\n");
     }
 
 
     private void getMostCommonArena(){
-        String location = DataAnalyzer.findMostCommonArena(file);
-        resultsArea.setText("Most Common Arena: " + location);
+        int index = Basketball.findMostCommonArena(file);
+        resultsArea.setText("Most Common Arena: " + getTeam(index).getLocation());
     }
 
     public void saveScreen(){
-        try {
-            
-        } catch (Exception e) {
+        counter++;
+        try{
+            int w = resultsArea.getWidth();
+            int h = resultsArea.getHeight();
+            int type =BufferedImage.TYPE_INT_ARGB;
+            BufferedImage sshot = new BufferedImage(w,h,type);
+
+            Graphics2D g2d = sshot.createGraphics();
+            resultsArea.paint(g2d);
+
+            File out = new File("Search "+ counter + ".png");
+            ImageIO.write(sshot,"png",out);
+            g2d.dispose();
+        }
+        catch(IOException e){
+            e.printStackTrace();
         }
     }
-
 
 
 
